6d57a832378eca2446fe3416c5585e5bb9a396cc
Shibam Dey <123672103+NekoSenpai69@users.noreply.github.com>
2024-02-22 20:14:28 +0000

    Lol

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.BASE_URL = `https://weeb-api-neko.onrender.com/weeb/api/`;
 global.prefix = "/";
 global.owner = ["919609421243","917047584741"];
-global.waitMesaage = "*_Wait A Minute_*";
+global.waitMesaage = "*Wait A Minute*";
 global.api_key = "mta3elttdphm8ryd";
 global.PORT = process.env.PORT ?? 10000;
